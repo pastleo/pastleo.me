ace8355f900a10d94d0207c88a439cbb5aac1e0f
PastLeo <chgu82837@gmail.com>
2019-02-17 22:02:50 +0800

    complete resume

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ document.getElementById('logo').onclick = function() {
   document.getElementById('logo').classList.toggle('revealed');
 }
 
+function openResume(event) {
+  document.getElementById('resume').classList.add('opened');
+}
+if (location.hash.includes('resume-opened')) {
+  openResume();
+}
+document.getElementById('open-resume').onclick = openResume
+
