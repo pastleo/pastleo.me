d4b46ca0d7184efec7c3abb98b32b7f24ce602e7
PastLeo <chgu82837@gmail.com>
2020-06-01 21:46:02 +0800

    add base og:image

--- components/HtmlHead.js
+++ components/HtmlHead.js
@@ -1,11 +1,14 @@
 import Head from 'next/head';
 
+import ogImageSrc from '../assets/og-image.jpg';
+
 const HtmlHead = ({ options }) => (
   <Head>
     <title key='title'>{ options.title || 'PastLeo' }</title>
     <meta key='description' name='Description' content='Blog, Introduction and Homepage of PastLeo' />
     <link key='favicon' rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
     <meta key='viewport' name='viewport' content='width=device-width, initial-scale=1' />
+    <meta key='og:image' name='og:image' content={ogImageSrc} />
   </Head>
 );
 
