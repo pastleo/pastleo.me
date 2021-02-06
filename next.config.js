527f43b0bcd10e2369e0281cb855f559c6db7266
PastLeo <chgu82837@gmail.com>
2021-02-06 17:23:11 +0800

    use frontmatter for post metadatas

--- next.config.js
+++ next.config.js
@@ -3,9 +3,18 @@ const withPlugins = require('next-compose-plugins');
 const withMDX = require('@next/mdx');
 const optimizedImages = require('next-optimized-images');
 
+const detectFrontmatter = require('remark-frontmatter');
+const extractFrontmatter = require('./lib/node/extractFrontmatter.js');
+
 module.exports = withPlugins([
   [withMDX({
     extension: /\.mdx?$/,
+    options: {
+      remarkPlugins: [
+        [detectFrontmatter, ['yaml']],
+        extractFrontmatter,
+      ],
+    },
   })],
   [optimizedImages, {}],
 ], {
