4ba07527eeba5853d62aff58ed59447b6f3c3b8c
PastLeo <chgu82837@gmail.com>
2021-02-24 01:48:08 +0800

    remove tailwindcss future flags
    they are already defaults

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   plugins: {
     tailwindcss: {
-      future: {
-        removeDeprecatedGapUtilities: true,
-        purgeLayersByDefault: true,
-      },
+      future: {},
       purge: [
         './components/**/*.js',
         './layouts/**/*.js',
